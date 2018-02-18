ca7238dfe7280d459acc22a21bcf0ac0bc1ae22b
Wolfram Kahl <kahl@cas.mcmaster.ca>
2018-02-18 13:29:37 -0500

    Added executable `NeksCat` for inspection of `store.ksv`.

---
+++ Network/Neks/NeksCat.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import System.IO (hPutStrLn, stderr)
+import System.Environment (getArgs)
+import Data.ByteString (ByteString)
+import Data.List (sort)
+import qualified Data.Map as Map (toList)
+
+import Control.Concurrent.STM (atomically)
+
+
+import Network.Neks.Disk (loadFrom)
+import Network.Neks.DataStore (DataStore, dump)
+
+type Store = DataStore ByteString ByteString
+
+main :: IO ()
+main = do
+        args <- getArgs
+        case args of
+                "--help" : _ -> hPutStrLn stderr instructions -- To be explicit
+                [storeFile] -> catDataStoreFile storeFile
+                [] -> catDataStoreFile "store.kvs"
+                _          -> hPutStrLn stderr instructions
+
+catDataStoreFile :: FilePath -> IO ()
+catDataStoreFile storeFile = do
+        loaded <- loadFrom storeFile
+        case loaded of
+                Nothing -> hPutStrLn stderr $ "Could not load from " ++ storeFile
+                Just (store :: Store) -> do
+                   maps <- atomically $ dump store
+                   let kvs = sort $ Map.toList =<< maps
+                       showKV (k, v) = shows k $ "\t\t" ++ show v
+                   mapM_ (putStrLn . showKV) kvs
+
+instructions :: String
+instructions = "Usage: NeksCat <opt-args>\n" ++
+               "<opt-args> can be empty or a store file name"
